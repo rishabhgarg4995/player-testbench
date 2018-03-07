b55601a2fd2584250dc8d8544dda64b547ce5a18
vagartu <tushar.agarwal@pearson.com>
2018-03-07 11:41:07 +0530

    including auth and logging

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+var basicAuth = require('express-basic-auth');
+const path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000 ;
 
-express()
+var app = express();
+
+app.use(basicAuth({
+    users: { 'compro': 'c0mpr0' },
+    challenge: true,
+    unauthorizedResponse: getUnauthorizedResponse,
+    realm: 'Leo Credential'
+}))
+
+function getUnauthorizedResponse(req) {
+    return req.auth ?
+        ('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected') :
+        'No credentials provided'
+}
+
+app
+    .use(logger('dev'))
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(cookieParser());
+
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
